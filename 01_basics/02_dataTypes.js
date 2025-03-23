874cf3a5d5e4765d08fddee5050ab82a2beafa81
aamirqazi14 <152431881+aamirqazi14@users.noreply.github.com>
2025-03-23 09:36:51 +0000

    basics dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use script"; //treat all JS code as newer version
+
+// alert(3+3) we are using nodejs, not browser
+
+console.log(3
+    +
+    3) // code readability should be high
+
+console.log("Aamir")
+
+let name = "Aamir"
+let age = 26
+let isloggedin = false
+
+// number => 2 to the power of 53
+// bigint (for big numbers like in stock market)
+// string => ""
+// boolean =>true/false
+// null => standalone value (mean you provide null instead of zero where required like in temp value)
+// undefined =>
+// symbol => unique (to uniquely identified)
+
+// object
+
+console.log(typeof name)
+console.log(typeof age)
+
+console.log(typeof isloggedin)
+
+console.log(typeof null)
+console.log(typeof undefined)
