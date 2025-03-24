2334dc5c4e47fbe039eb03616a1df3b5e0a5d490
aamirqazi14 <152431881+aamirqazi14@users.noreply.github.com>
2025-03-24 15:57:13 +0000

    data summary

---
+++ 01_basics/datatype_summary.js
@@ -0,0 +1,43 @@
+// Primitive Datatype :
+
+// Note : primitive type are called by value, when you one value to another the original data is not given, copy of it provided
+
+// 7 Types : String, Number, Boolean, Null, undefined, Symbol, BigInt 
+
+// undefined : variable declared, memory space declared, value is yet to be assign
+
+
+const score = 100;
+const scoreValue = 100.3; // it does not have separately float as datatype, this is also number, float is also come under number 
+
+const isLoggedIn = false;
+const outSideTemp = null;
+let useremail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+
+// console.log(id === anotherId);
+
+const bigNumber = 6463143131413144451n;
+
+// Reference Datatype (Non Primitive) :
+
+// Note : If you want to master JavaStript, Master JavaStript Objects & Browser Web Events
+
+// Array, objects, Functions
+
+const heros = ["Virat Kohli", "Phil Salt", "Krunal Pandya"];
+let myObj = {
+    name : "Aamir",
+    age : 22,
+} // between this curly brackets is object // and we can store objects in variable
+
+// datatype can be string, number, boolean, function, array or it can be object
+
+const myFucntion = function(){ // we can store function defination in variable 
+    console.log("Hello world");
+}
+
+console.log(typeof bigNumber);
