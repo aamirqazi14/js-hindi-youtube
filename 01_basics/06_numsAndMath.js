801ed0be2d3732dedeb529e690b249483f5fbd87
aamirqazi14 <152431881+aamirqazi14@users.noreply.github.com>
2025-03-26 10:19:02 +0000

    maths & num functions

---
+++ 01_basics/06_numsAndMath.js
@@ -0,0 +1,42 @@
+ const score = 400
+//  console.log(score);
+
+ const balance = new Number(100.51664646)
+//  console.log(balance);
+
+//  console.log(balance.toString().length);
+//  console.log(balance.toFixed(2));
+
+ const otherNumber = 123.8699;
+
+//  console.log(otherNumber.toPrecision(4))
+
+ const hundreds = 1000000
+
+//  console.log(hundreds.toLocaleString('en-IN'));
+
+//  +++++++++++++++++++++++++++++++++++++++ Maths ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//  console.log(Math);
+//  console.log(Math.abs(-10));
+//  console.log(Math.round(4.6));
+//  console.log(Math.ceil(4.2));
+//  console.log(Math.floor(4.9));
+//  console.log(Math.sqrt(16));
+//  console.log(Math.max(4,3,6,8));
+//  console.log(Math.min(4,3,6,8));
+
+
+// console.log(Math.random() );
+// console.log((Math.random()*10)+1)
+// console.log((Math.random()*10)+1)
+// Math.random will generate 0 to 1 random value, where 0.05 is also one of the value so
+// 0.05 * 10 = 0.5 // that's why we increse it with 1  //  0.5 + 1 = 1.5 roundOff 1
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random()*(max-min + 1)) +min);
+
+
+
