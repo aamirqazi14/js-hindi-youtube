846242ae549420ca6a1982e3cf7a4ab4402cf9e6
aamirqazi14 <152431881+aamirqazi14@users.noreply.github.com>
2025-04-02 09:15:55 +0000

    basics of functions completed

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -24,7 +24,7 @@ function addTwoNumbers(number1,number2){
 // const result = addTwoNumbers(6,2) 
 // console.log(result)
 
-console.log(addTwoNumbers(6,2))
+// console.log(addTwoNumbers(6,2))
 
 function loginUser(username){
     if(!username){
@@ -34,5 +34,40 @@ function loginUser(username){
     return `${username} just logged in`
 }
 
-console.log(loginUser())
+// console.log(loginUser())
 // console.log(loginUser("Aamir"))
+
+
+// **********************************************************************************************************************
+
+
+// function calculateCartPrice(val1, val2, ...num1){
+//     return num1
+// }
+
+// console.log(calculateCartPrice(200,100,400,600))
+
+const user = {
+    username : "Aamir",
+    price : 999
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and product price is ${anyobject.price}`)
+}
+
+// handleObject(user)
+
+handleObject({
+    username : "Qazi",
+    price : 499
+})
+
+const numberArray = [100,200,300,400,500,600]
+
+function returnThirdValue(anyArray){
+    return anyArray[2]
+}
+
+// console.log(returnThirdValue(numberArray))
+console.log(returnThirdValue([100,200,300,400,500,600]))
