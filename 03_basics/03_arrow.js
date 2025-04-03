3c885ae174a8676260d7e2d3f172a6591e0be810
aamirqazi14 <152431881+aamirqazi14@users.noreply.github.com>
2025-04-03 09:57:23 +0000

    arrow function

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,53 @@
+// const user = {
+//     username : "Aamir",
+//     product_price : 899,
+
+//     welcomeMessage : function(){
+//         console.log(`${this.username} , Welcome to the website`);
+//         console.log(this);
+//     }
+// }
+
+
+// user.welcomeMessage()
+// user.username = "Qazi"
+// user.welcomeMessage()
+
+
+// console.log(this)
+
+// function chai(){
+//     let username = "Aamir"
+//     console.log(this.username)
+// }
+
+// const chai = function (){
+//         let username = "Aamir"
+//         console.log(this.username)
+//     }
+
+
+// const chai = () => {
+//     let name = "Aamir"
+//     console.log(this.name)
+//     // console.log(this)
+// }
+// chai()
+
+
+// const addTwo = function(num1,num2) {
+//     return num1+num2
+// }
+
+// const addTwo = (num1,num2) => num1+num2 
+
+// const addTwo = (num1,num2) => (num1+num2) 
+
+// console.log(addTwo(5,8))
+
+// const addTwo = (num1,num2) => ({username : "Aamir"})
+
+// console.log(addTwo(5,8))
+
+const myArray =  [1,2,3,4,5,6]
+
