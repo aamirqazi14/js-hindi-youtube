fa60e53588f42328ce6bd905a27a74c8cb17b1d7
aamirqazi14 <152431881+aamirqazi14@users.noreply.github.com>
2025-04-03 10:33:42 +0000

    comments

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -19,35 +19,38 @@
 // function chai(){
 //     let username = "Aamir"
 //     console.log(this.username)
+//         console.log(this) // output is global object values
+
 // }
 
 // const chai = function (){
 //         let username = "Aamir"
 //         console.log(this.username)
+//         console.log(this) // output is global object values
 //     }
 
 
 // const chai = () => {
 //     let name = "Aamir"
 //     console.log(this.name)
-//     // console.log(this)
+//     console.log(this) // output is undefined
 // }
 // chai()
 
 
-// const addTwo = function(num1,num2) {
-//     return num1+num2
+// const addTwo = (num1,num2) =>  {
+//     return num1+num2 // explicitly return keyword is used
 // }
 
-// const addTwo = (num1,num2) => num1+num2 
+// const addTwo = (num1,num2) => num1+num2  // it is done implicitly 
 
-// const addTwo = (num1,num2) => (num1+num2) 
-
-// console.log(addTwo(5,8))
-
-// const addTwo = (num1,num2) => ({username : "Aamir"})
+// const addTwo = (num1,num2) => (num1+num2) // same here 
 
 // console.log(addTwo(5,8))
 
-const myArray =  [1,2,3,4,5,6]
+// const addTwo = (num1,num2) => ({username : "Aamir"}) // parentheses can used for returning object 
+
+// console.log(addTwo(5,8))
+
+// const myArray =  [1,2,3,4,5,6]
 
