b1b7f43dd3ca9dbe7e81cad0059a8bbbdd8b7a7c
aamirqazi14 <aamirq9999@gmail.com>
2025-04-17 11:58:23 +0530

    used foreach in alternate of map

--- 05_iterations/seven.js
+++ 05_iterations/seven.js
@@ -3,10 +3,26 @@ const mynumbers = [1,2,3,4,5,6,7,8,9,10]
 // const newNums = mynumbers.map( (items) => { return items * 2})
 
 
-const newNums = mynumbers
-    .map((items) => items * 10 )
-    .map((items) => items + 1)
-    .filter((items) => items > 40)
+// const newNums = mynumbers
+//     .map((items) => items * 10 )
+//     .map((items) => items + 1)
+//     .filter((items) => items > 40)
  
 
-console.log(newNums)
+// console.log(newNums)
+
+
+// trying the same thing with forEach
+const newNum = mynumbers.forEach(num => {
+        if(num >= 1){
+            let result = num * 10 
+            if (result > 40){
+                let final = result + 1
+
+                //  console.log(final)
+                return final
+            }
+        }
+    });
+
+    console.log(newNum)
